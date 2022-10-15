f73bb512451cb8d7199acea44a1500ded05ba085
sktp06 <kantaporn_s@cmu.ac.th>
2022-10-16 00:45:08 +0700

    4.8

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -7,7 +7,7 @@ export default {
         password: user.password
       })
       .then((response) => {
-        localStorage.setItem('token', response.data.token)
+        localStorage.setItem('user', JSON.stringify(response.data.user))
         return Promise.resolve(response.data)
       })
       .catch((error) => {
