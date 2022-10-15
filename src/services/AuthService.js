ec89fc5554e05ac4d38bd4c6cc692fa0d527e57b
sktp06 <kantaporn_s@cmu.ac.th>
2022-10-16 00:13:06 +0700

    3.7

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,23 +1,26 @@
-import axios from "axios";
+import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: process.env.VUE_APP_BACKEND_URL,
-    withCredentials: false,
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
-  export default {
-    login(user){
-        return apiClient.post('/auth',{
-          username: user.username,
-          password: user.password
-        }).then((response)=>{
-          localStorage.setItem('token',response.data.token)
-          return Promise.resolve(response.data)
-        }).catch((error)=>{
-          return Promise.reject(error)
-        })
-    }
+  baseURL: process.env.VUE_APP_BACKEND_URL,
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
   }
+})
+export default {
+  login(user) {
+    return apiClient
+      .post('/auth', {
+        username: user.username,
+        password: user.password
+      })
+      .then((response) => {
+        localStorage.setItem('token', response.data.token)
+        return Promise.resolve(response.data)
+      })
+      .catch((error) => {
+        return Promise.reject(error)
+      })
+  }
+}
