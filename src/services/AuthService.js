c13867a21c3d2862c265605c4102131fb0f1f04a
sktp06 <kantaporn_s@cmu.ac.th>
2022-10-16 01:13:41 +0700

    4.11

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,4 +1,5 @@
 import apiClient from '@/services/AxiosClient.js'
+import GStore from '@/store'
 export default {
   login(user) {
     return apiClient
@@ -7,7 +8,9 @@ export default {
         password: user.password
       })
       .then((response) => {
+        localStorage.setItem('token', response.data.token)
         localStorage.setItem('user', JSON.stringify(response.data.user))
+        GStore.currentUser = response.data.user
         return Promise.resolve(response.data)
       })
       .catch((error) => {
@@ -17,6 +20,7 @@ export default {
   logout() {
     localStorage.removeItem('token')
     localStorage.removeItem('user')
+    GStore.currentUser = null
   },
   getUser() {
     return JSON.parse(localStorage.getItem('user'))
