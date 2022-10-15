29bc9d1a6cd092433c5a846c53c48673cc0f64e9
sktp06 <kantaporn_s@cmu.ac.th>
2022-10-16 01:33:57 +0700

    5.2

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@
   <nav>
     <router-link :to="{ name: 'EventList' }">Home</router-link> |
     <router-link :to="{ name: 'about' }">About</router-link> |
-    <span v-if="isAdmin">
+    <span>
       <router-link :to="{ name: 'AddEvent' }"> New Event</router-link>
     </span>
   </nav>
