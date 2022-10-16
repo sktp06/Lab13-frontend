1a8a12cb907ff3c8ba47cde235c9e9fd5e696c14
sktp06 <kantaporn_s@cmu.ac.th>
2022-10-16 13:58:17 +0700

    6.8 fix

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@
   </div>
   <nav>
     <router-link :to="{ name: 'EventList' }">Home</router-link> |
-    <router-link :to="{ name: 'about' }">About</router-link> |
+    <router-link :to="{ name: 'about' }">About</router-link>
     <span v-if="isAdmin">
       |
       <router-link :to="{ name: 'AddEvent' }"> New Event</router-link>
