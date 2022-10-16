053766ea3f8df9033a780aec678126366d13c33b
sktp06 <kantaporn_s@cmu.ac.th>
2022-10-16 16:58:26 +0700

    deploy2

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_URL,
+  baseURL: 'http://3.87.75.115:8999',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
